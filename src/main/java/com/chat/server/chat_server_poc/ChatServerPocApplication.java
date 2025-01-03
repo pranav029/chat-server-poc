62308ba279b0abcb89d3b53fdfc65ef9aa3f24b3
pranav029 <pranavmani.naman@gmail.com>
2025-01-03 16:10:33 +0530

    Init Project

---
+++ src/main/java/com/chat/server/chat_server_poc/ChatServerPocApplication.java
@@ -0,0 +1,13 @@
+package com.chat.server.chat_server_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServerPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/server/chat_server_poc/ChatServerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.server.chat_server_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
